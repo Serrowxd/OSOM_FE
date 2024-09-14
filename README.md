05f7b4c385c698cfe6a717c64ebbc79b6e92ab65
Kevin <serrowpls@gmail.com>
2024-09-14 16:47:01 -0500

    styling changes and readme update

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # OSOM_FE
+
 Out of sight, out of mind
+
+### Development Notes
+
+Current wants, in no particular order:
+
+- a "today" option that is easy to click, so we can assign a date easily
+  - optionally automatic for when the user does not add a date, it should default to today's date.
+- "mark as completed" as an option. Either a checkbox that turns the box green, or an option in the edit modal.
+- Back-end implementation to store notes and user data.
+  - Look into having a local storage version that we make a copy off, minimize cloud usage.
+- Connect the API to ChatGPT and create a prompt list for optimizing tasks.
+  - This should be an option that the user can select by saying "Tackle the day" or something similar
+  - Upon selecting the action, it will generate a return from ChatGPT that will give you a tl;dr on how to attack the list.
+  - The user can re-generate this every (5?) minutes, and we will store it on their profile to be shown whenever they login.
+  - Add the ability to clear the prompt.
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import TaskList from './components/TaskList';
-import AddTaskModal from './components/AddTaskModal';
-import PriorityMessage from './components/PriorityMessage';
-import CalendarComponent from './components/CalendarComponent';
+import React, { useState } from "react";
+import TaskList from "./components/TaskList";
+import AddTaskModal from "./components/AddTaskModal";
+import PriorityMessage from "./components/PriorityMessage";
+import CalendarComponent from "./components/CalendarComponent";
 import Sidebar from "./components/Sidebar";
 import EditTaskModal from "./components/EditTaskModal";
 
@@ -11,69 +11,75 @@ function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [taskToEdit, setTaskToEdit] = useState(null);
-  
+
   // Handlers
   const handleAddTask = () => setIsModalOpen(true);
-  
+
   const handleSaveTask = (newTask) => {
     setTasks([...tasks, newTask]);
     setIsModalOpen(false);
   };
-  
+
   const handleRemoveTask = (taskId) => {
     setTasks(tasks.filter((task) => task.id !== taskId));
   };
-  
+
   const handleCloseModal = () => setIsModalOpen(false);
-  
+
   const handleTaskClick = (taskId) => {
     const task = tasks.find((task) => task.id === taskId);
     setTaskToEdit(task);
     setIsEditModalOpen(true);
   };
-  
+
   const handleSaveEditedTask = (editedTask) => {
     setTasks(
-      tasks.map((task) => (task.id === editedTask.id ? editedTask : task))
+      tasks.map((task) => (task.id === editedTask.id ? editedTask : task)),
     );
     setIsEditModalOpen(false);
     setTaskToEdit(null);
   };
-  
+
   const handleCloseEditModal = () => {
     setIsEditModalOpen(false);
     setTaskToEdit(null);
   };
-  
+
   return (
     <div className="h-screen flex flex-col md:flex-row">
       <Sidebar />
       {/* Left Side: Task List */}
       <div className="w-full md:w-2/3 flex flex-col overflow-hidden">
         <div className="flex-1 p-6 overflow-y-auto">
-          <TaskList tasks={tasks} onAddTask={handleAddTask} onRemoveTask={handleRemoveTask} onTaskClick={handleTaskClick} />
+          <TaskList
+            tasks={tasks}
+            onAddTask={handleAddTask}
+            onRemoveTask={handleRemoveTask}
+            onTaskClick={handleTaskClick}
+          />
         </div>
       </div>
-      
+
       {/* Right Side: Priority Message and Calendar */}
       <div className="w-full md:w-1/3 flex flex-col">
-        {/* Top Right Square: Priority Message */}
-        <div className="flex-1 p-6">
-          <PriorityMessage/>
-        </div>
-        {/* Bottom Right Square: Calendar */}
+        {/* Calendar */}
         <div className="flex-1 p-6 border-t">
-          <CalendarComponent tasks={tasks}/>
+          <CalendarComponent tasks={tasks} />
+        </div>
+
+        {/* Priority Message */}
+        <div className="flex-1 p-6">
+          <PriorityMessage />
         </div>
       </div>
-      
+
       {/* Add Task Modal */}
       <AddTaskModal
         isOpen={isModalOpen}
         onClose={handleCloseModal}
         onSave={handleSaveTask}
       />
-      
+
       {/* Edit Task Modal */}
       {taskToEdit && (
         <EditTaskModal
